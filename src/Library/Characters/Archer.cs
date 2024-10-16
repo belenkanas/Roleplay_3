17b442f84e49b49106605180b112177763b0ca75
Valentina <valentina.blanco@correo.ucu.edu.uy>
2024-10-16 10:32:12 -0300

    Clase abstracta 2

--- src/Library/Characters/Archer.cs
+++ src/Library/Characters/Archer.cs
@@ -3,12 +3,14 @@ namespace Ucu.Poo.RoleplayGame;
 
 public class Archer: Character
 {
-    public Archer(string name)
-    : base(name)
+
+    public Archer(string name) : base(name)
     {
         this.Name = name;
 
         this.AddItem(new Bow());
         this.AddItem(new Helmet());
     }
+
+   
 }
