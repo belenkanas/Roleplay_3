9debbf7a9c7e4dc21332094551d7b20d83aa9bee
belenkanas <maria.kanas@correo.ucu.edu.uy>
2024-10-18 11:10:52 -0300

    Comentarios Encounter

--- src/Library/Encounter.cs
+++ src/Library/Encounter.cs
@@ -2,11 +2,19 @@ using System.Net.Sockets;
 
 namespace Ucu.Poo.RoleplayGame
 {
+    //Mediante esta clase se va a definir la partida entre héroes y villano. En primer lugar se va a presentar un equipo de cada uno
+    //(una lista) en la cual uno juega con el otro respectivamente (respetando sus índices en la lista).
+    //Esta clase será utilizada en Program, gracias a los principios de delegación.
     public class Encounter
     {
+        //Poniendo la lista privada de Heroes nos aseguramos de que no se modifique mediante la partida
         private List<Heroes> heroes;
+        
+        //Lo mismo con la lista de villanos
         private List<Villains> villains;
 
+        //El constructor necesita conocer a los personajes y debe asegurar que por lo menos haya uno en cada equipo, de lo contrario
+        //no se podrá hacer la partida.
         public Encounter(List<Heroes> heroes, List<Villains> villains)
         {
             if (heroes.Count == 0 || villains.Count == 0)
@@ -25,6 +33,8 @@ namespace Ucu.Poo.RoleplayGame
             
         }
 
+        //Primero atacan los villanos, se analiza uno por uno la lista del villano y se busca al héroe con mismo índice
+        //para luego calcular el daño causado y dárselo al contrincante.
         private void VillainsAttack()
         {
             int indiceHeroeAtacado = 0;
@@ -50,6 +60,8 @@ namespace Ucu.Poo.RoleplayGame
             }
         }
 
+        //Los héroes atacan después de los villanos, por lo que, además de usar una lógica parecida, primero se tiene que
+        //asegurar que el héroe siga vivo. En caso de que derrote por completo al enemigo, recibe los puntos de victoria.
         private void HeroesAttack()
         {
             foreach (Heroes hero in heroes)
